89233f173c8e798faadeac57ce9829db6dfdf631
Prajwal <prajwal17022000@gmail.com>
2026-02-05 22:47:02 +0530

    numpy array

--- Array/array_numpy.py
+++ Array/array_numpy.py
@@ -1,9 +1,60 @@
 import numpy as np
 
-arr = np.array([1,2,3,4.5])
+arr = np.array([1,2,3,4],dtype= float)
 print(arr)
+
+val = np.linspace(10,20,5)
+val0 = np.arange(10,20,2)
+val1 = np.logspace(10,20,2)
+val2 = np.zeros(10)
+val3 = np.ones(3)
+val4 = np.full(10,5)
 
 print()
 
 for x in arr:
     print(x,end=" ")
+
+print()
+
+for i in val:
+    print(i,end=" ")
+
+print()
+
+for i in val0:
+    print(i,end=" ")
+
+print()
+
+for i in val1:
+    print(i,end=" ")
+
+print()
+
+for i in val2:
+    print(i,end=" ")
+
+
+print()
+
+for i in val3:
+    print(i,end=" ")
+
+print()
+
+for i in val4:
+    print(i,end=" ")
+print()
+
+zero = np.array(10)
+print(zero)
+
+one = np.array([1,2,3,4])
+print(one)
+
+two = np.array([[1,2],[3,4]])
+print(two)
+
+three = np.array([[[1,2],[3,4],[5,6]]])
+print(three)
